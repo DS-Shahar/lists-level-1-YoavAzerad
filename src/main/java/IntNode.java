8d7ede1d392caeaab76c7cbd4a2dda61467413e8
YoavAzerad <yoavazerad2008@gmail.com>
2024-11-30 23:57:22 +0200

    לא הספקתי לעשות את 3 ו7

--- src/main/java/IntNode.java
+++ src/main/java/IntNode.java
@@ -1,5 +1,3 @@
-package com.shaharyi.node;
-
 public class IntNode {
   private int value;
   private IntNode next;
@@ -37,4 +35,86 @@ public class IntNode {
   public String toString() {
     return value + " --> " + next;
   }
+
+  public IntNode ex1(int [] array) {
+    IntNode list = new IntNode(0);
+    IntNode listnext = list;
+    for (int i=0; i<array.length; i++) {
+      listnext.next = new IntNode(array[i]);
+      listnext = listnext.next;
+    }
+    return list;
+
+  }
+
+  public void ex2(IntNode list1) {
+    while (list1 != null) {
+        System.out.println(list1.getValue()); 
+        list1 = list1.getNext(); 
+    }
+  }
+
+  public void ex2recursion(IntNode list1) {
+    if (list1 != null) {
+      System.out.println(list1.getValue()); 
+      ex2recursion(list1.getNext());
+    }
+  }
+
+  public void ex2reverse(IntNode list1) {
+    if (list1 != null) {
+      ex2reverse(list1.getNext()); 
+      System.out.println(list1.getValue());
+    }
+  }
+
+  public void ex4(IntNode list1) {
+    while (list1 != null) {
+      if (list1.getValue()%2==0) {
+        System.out.println("ex4 " + list1.getValue());
+      }
+      list1 = list1.getNext();
+    }
+  }
+
+  public boolean ex5(IntNode list1, int num) {
+    while (list1 != null) {
+      if (list1.getValue()==num) {
+        return true;
+      }
+      list1 = list1.getNext();
+    }
+    return false;
+  }
+
+  public boolean ex5recursionT(IntNode list1, int num) {
+    if (list1==null) {
+      return false;
+    }
+    if (list1.getValue()==num) {
+      return true;
+    }
+    return ex5recursionT(list1.getNext(), num);
+  }
+
+  public IntNode ex6(IntNode list1, int num) {
+    IntNode p = new IntNode(-1, list1);
+    list1 = p;
+    while (p.hasNext()) {
+      IntNode t;
+      if (p.getNext().getValue()==num) {
+        t = p.getNext();
+        p.setNext(p.getNext().getNext());
+        t.setNext(null);
+        return list1.getNext();
+      }
+      p = p.getNext();
+    }
+    return list1.getNext();
+  }
+
+
+
+
+  
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    int [] array = {1, 2, 3, 4, 5};
+    IntNode node = new IntNode(0);
+    IntNode list = node.ex1(array);
+    System.out.println(list);
+    node.ex2(list);
+    node.ex2recursion(list);
+    node.ex2reverse(list);
+    node.ex4(list);
+    System.out.println(node.ex5(list, 4));
+    System.out.println(node.ex5recursionT(list, 4));
+    System.out.println("ex6");
+    System.out.println(node.ex6(list, 2));
   }
 }
